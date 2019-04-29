1b8073f3dffd8603a78dccfa0366aff999f81df0
Markus Repnak Jacobsen <markusjacobsen@hotmail.com>
2019-04-29 16:15:22 +0200

    Inkluderet nogle nye metoder

--- BenytKalahaSpil/src/kalaha/NetvaerkKommunikation.java
+++ BenytKalahaSpil/src/kalaha/NetvaerkKommunikation.java
@@ -3,7 +3,40 @@
  */
 package kalaha;
 
+import java.net.DatagramSocket;
+        
 public class NetvaerkKommunikation
 {
+    private int portNummer = 42069;
+    private String spillerIPAdresser[];
     
+    public NetvaerkKommunikation(String initspillerIPAdresser[])throws Exception
+    {
+        DatagramSocket kommunikationsSocket = new DatagramSocket(portNummer);
+        setspillerIPAdresser(initspillerIPAdresser);
+
+    }
+    
+    public void setspillerIPAdresser(String initspillerIPAdresser[])
+    {
+        if (spillerIPAdresser.length == initspillerIPAdresser.length)
+        {
+            spillerIPAdresser = initspillerIPAdresser;
+        }
+        else
+        {
+            System.out.println("Fejl, maengderne af IP-adresser stemmer ikke overens."); //Implementer log?
+        }
+    }
+        
+    public String[] getspillerIPAdresser()
+    {
+        return spillerIPAdresser;
+    }
+    
+    public void sendSpilStatus()
+    {
+        
+    }
+   
 }
