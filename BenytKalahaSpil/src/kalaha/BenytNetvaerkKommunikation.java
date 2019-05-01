27fc3a20f37964263e3c0c2e3ec401d1f6416c26
Markus Repnak Jacobsen <markusjacobsen@hotmail.com>
2019-05-01 09:56:35 +0200

    Implementeret modtag og send data test med en enkelt modtager

--- BenytKalahaSpil/src/kalaha/BenytNetvaerkKommunikation.java
+++ BenytKalahaSpil/src/kalaha/BenytNetvaerkKommunikation.java
@@ -3,7 +3,38 @@
  */
 package kalaha;
 
+import java.net.InetAddress;
+import java.util.*;
+
 public class BenytNetvaerkKommunikation
 {
-    
+
+    public static void main(String[] args) throws Exception
+    {
+        while (true)
+        {
+            Scanner input = new Scanner(System.in);
+            System.out.println("Din IP adresse er: " + InetAddress.getLocalHost().getHostAddress().toString());
+            System.out.println("Vil du modtager eller sende data? [S/M]");
+            if (input.nextLine() == "S")
+            {
+                System.out.println("indtast IP adresse på din modtager på det samme netvaerk:");
+                String IPAdresser[] = new String[1];
+                IPAdresser[0] = input.nextLine(); //10.16.165.156
+                NetvaerkKommunikation sendData = new NetvaerkKommunikation(IPAdresser);
+                sendData.sendSpilStatus("luder");
+            }
+            else if (input.nextLine() == "M")
+            {
+                String IPAdresser[] = new String[1];
+                IPAdresser[0] = InetAddress.getLocalHost().getHostAddress().toString();
+                NetvaerkKommunikation modtagData = new NetvaerkKommunikation(IPAdresser);
+                System.out.println(modtagData.modtagSpilStatus());
+            }
+            else
+            {
+                System.out.println("Forkert input.");
+            }
+        }
+    }
 }
