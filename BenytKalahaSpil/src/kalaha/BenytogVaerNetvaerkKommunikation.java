2901be2c15f2b912e1077fdc2e59f17c0dfe0c87
Markus Repnak Jacobsen <markusjacobsen@hotmail.com>
2019-05-08 09:03:58 +0200

    yderligere forsøgt

--- BenytKalahaSpil/src/kalaha/BenytogVaerNetvaerkKommunikation.java
+++ BenytKalahaSpil/src/kalaha/BenytogVaerNetvaerkKommunikation.java
@@ -20,28 +20,38 @@ public class BenytogVaerNetvaerkKommunikation
     {
         while (true)
         {
-            Scanner input = new Scanner(System.in);
-            InetAddress IPAdresse = InetAddress.getByName("10.16.165.156");
-            System.out.println("Din IP adresse er: " + InetAddress.getLocalHost().getHostAddress()); //Java2s network 
-            System.out.println("Vil du modtager eller sende data? [S/M]");
-            String kommando = input.nextLine();
-
-            if (kommando.equals("S") || kommando.equals("s"))
+            try
             {
-                Socket SMSocket = new Socket(IPAdresse, 42069);
+                Scanner input = new Scanner(System.in);
+                InetAddress IPAdresse = InetAddress.getByName("10.16.165.156");
+                System.out.println("Din IP adresse er: " + InetAddress.getLocalHost().getHostAddress()); //Java2s network 
+                System.out.println("Vil du modtager eller sende data? [S/M]");
+                Socket SMSocket;
+                SMSocket = new Socket(IPAdresse, 42069);
                 PrintWriter ud = new PrintWriter(SMSocket.getOutputStream(), true);
                 BufferedReader ind = new BufferedReader(new InputStreamReader(SMSocket.getInputStream()));
-            }
+                String kommando = input.nextLine();
 
-            else if (kommando.equals("M") || kommando.equals("m"))
+                if (kommando.equals("S") || kommando.equals("s"))
+                {
+                    ud.append("hej");
+                }
+
+                else if (kommando.equals("M") || kommando.equals("m"))
+                {
+                    System.out.print(ind.read());
+                }
+
+                else
+                {
+                    System.out.println("Forkert input.");
+                }
+            }
+            catch (Exception e)
             {
-
+                e.printStackTrace();
             }
 
-            else
-            {
-                System.out.println("Forkert input.");
-            }
         }
     }
 }
