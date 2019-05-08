8f442345615d017cac2a7dd3d8a06032d0577521
Markus <Markus@DESKTOP-NNP52VT>
2019-05-08 19:25:19 +0200

    Fixede koden Dog skal der specificeres om der modtages eller sendes data

--- BenytKalahaSpil/src/kalaha/BenytogVaerNetvaerkKommunikation.java
+++ BenytKalahaSpil/src/kalaha/BenytogVaerNetvaerkKommunikation.java
@@ -23,69 +23,40 @@ public class BenytogVaerNetvaerkKommunikation
             try
             {
                 Scanner input = new Scanner(System.in);
-                InetAddress IPAdresse = InetAddress.getByName("10.16.164.59");
+                InetAddress IPAdresse = InetAddress.getByName("192.168.0.20");
                 System.out.println("Din IP adresse er: " + InetAddress.getLocalHost().getHostAddress()); //Java2s network 
                 System.out.println("Vil du modtager eller sende data? [S/M]");
                 //while (true)
                 //{
                 String kommando = input.nextLine();
 
-                
+                if (kommando.equals("S") || kommando.equals("s"))
+                {
                     Socket SMSocket = new Socket(IPAdresse, 42069);
                     PrintWriter ud = new PrintWriter(SMSocket.getOutputStream(), true);
-                    BufferedReader ind = new BufferedReader(new InputStreamReader(SMSocket.getInputStream()));
-
-                    if (kommando.equals("S") || kommando.equals("s"))
+                    ud.println("i t j u s t w o r k s");
+                }
+                else
+                {
+                    if (kommando.equals("M") || kommando.equals("m"))
                     {
-                        ud.append("hej");
-                    }
-
-                    else if (kommando.equals("M") || kommando.equals("m"))
-                    {
-                        System.out.print(ind.read());
+                        ServerSocket SS = new ServerSocket(42069);
+                        Socket SMSocket = SS.accept();
+                        BufferedReader ind = new BufferedReader(new InputStreamReader(SMSocket.getInputStream()));
+                        System.out.println(ind.readLine());
                     }
 
                     else
                     {
                         System.out.println("Forkert input.");
                     }
-                 
-                /*
-                if (kommando.equals("S") || kommando.equals("s"))
-                {
-                    DatagramSocket ds = new DatagramSocket();
-                    DatagramPacket dp = new DatagramPacket("hej".getBytes(), "hej".length(), IPAdresse, 42069);
-                    ds.send(dp);
-                    ds.close();
                 }
 
-                else if (kommando.equals("M") || kommando.equals("m"))
-                {
-                    DatagramSocket ds = new DatagramSocket(42069);
-                    byte[] buf = new byte[1024];
-                    DatagramPacket dp = new DatagramPacket(buf, 1024);
-                    while (true)
-                    {
-                        ds.receive(dp);
-                        //String str = new String(dp.getData(), 0, dp.getLength());
-                        System.out.println(dp.getSocketAddress());
-                    }
-                    ds.close();
-                }
-
-
-                else
-                {
-                    System.out.println("Forkert input.");
-                }
-                }
-*/
             }
             catch (Exception e)
             {
                 e.printStackTrace();
             }
-
         }
     }
 }
