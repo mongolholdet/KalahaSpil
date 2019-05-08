4b5d122ad8132c8fb969bb6965744d2cd908a821
Markus Repnak Jacobsen <markusjacobsen@hotmail.com>
2019-05-08 09:28:56 +0200

    forsøg med datagram

--- BenytKalahaSpil/src/kalaha/BenytogVaerNetvaerkKommunikation.java
+++ BenytKalahaSpil/src/kalaha/BenytogVaerNetvaerkKommunikation.java
@@ -23,15 +23,17 @@ public class BenytogVaerNetvaerkKommunikation
             try
             {
                 Scanner input = new Scanner(System.in);
-                InetAddress IPAdresse = InetAddress.getByName("10.16.165.156");
+                InetAddress IPAdresse = InetAddress.getByName("10.16.164.59");
                 System.out.println("Din IP adresse er: " + InetAddress.getLocalHost().getHostAddress()); //Java2s network 
                 System.out.println("Vil du modtager eller sende data? [S/M]");
-                while (true)
-                {
+                //while (true)
+                //{
+                    String kommando = input.nextLine();
+
+                    /*
                     Socket SMSocket = new Socket(IPAdresse, 42069);
                     PrintWriter ud = new PrintWriter(SMSocket.getOutputStream(), true);
                     BufferedReader ind = new BufferedReader(new InputStreamReader(SMSocket.getInputStream()));
-                    String kommando = input.nextLine();
 
                     if (kommando.equals("S") || kommando.equals("s"))
                     {
@@ -47,7 +49,31 @@ public class BenytogVaerNetvaerkKommunikation
                     {
                         System.out.println("Forkert input.");
                     }
-                }
+                     */
+                    if (kommando.equals("S") || kommando.equals("s"))
+                    {
+                        DatagramSocket ds = new DatagramSocket();
+                        DatagramPacket dp = new DatagramPacket("hej".getBytes(), "hej".length(), IPAdresse, 42069);
+                        ds.send(dp);
+                        ds.close();
+                    }
+
+                    else if (kommando.equals("M") || kommando.equals("m"))
+                    {
+                        DatagramSocket ds = new DatagramSocket(42069);
+                        byte[] buf = new byte[1024];
+                        DatagramPacket dp = new DatagramPacket(buf, 1024);
+                        ds.receive(dp);
+                        String str = new String(dp.getData(), 0, dp.getLength());
+                        System.out.println(str);
+                        ds.close();
+                    }
+
+                    else
+                    {
+                        System.out.println("Forkert input.");
+                    }
+                //}
             }
             catch (Exception e)
             {
