28c3e7f0bd282b30b2a25145b70b23e76b07adf8
Markus Repnak Jacobsen <markusjacobsen@hotmail.com>
2019-05-08 09:53:55 +0200

    tcp test

--- BenytKalahaSpil/src/kalaha/BenytogVaerNetvaerkKommunikation.java
+++ BenytKalahaSpil/src/kalaha/BenytogVaerNetvaerkKommunikation.java
@@ -28,9 +28,9 @@ public class BenytogVaerNetvaerkKommunikation
                 System.out.println("Vil du modtager eller sende data? [S/M]");
                 //while (true)
                 //{
-                    String kommando = input.nextLine();
+                String kommando = input.nextLine();
 
-                    /*
+                
                     Socket SMSocket = new Socket(IPAdresse, 42069);
                     PrintWriter ud = new PrintWriter(SMSocket.getOutputStream(), true);
                     BufferedReader ind = new BufferedReader(new InputStreamReader(SMSocket.getInputStream()));
@@ -49,31 +49,37 @@ public class BenytogVaerNetvaerkKommunikation
                     {
                         System.out.println("Forkert input.");
                     }
-                     */
-                    if (kommando.equals("S") || kommando.equals("s"))
-                    {
-                        DatagramSocket ds = new DatagramSocket();
-                        DatagramPacket dp = new DatagramPacket("hej".getBytes(), "hej".length(), IPAdresse, 42069);
-                        ds.send(dp);
-                        ds.close();
-                    }
+                 
+                /*
+                if (kommando.equals("S") || kommando.equals("s"))
+                {
+                    DatagramSocket ds = new DatagramSocket();
+                    DatagramPacket dp = new DatagramPacket("hej".getBytes(), "hej".length(), IPAdresse, 42069);
+                    ds.send(dp);
+                    ds.close();
+                }
 
-                    else if (kommando.equals("M") || kommando.equals("m"))
+                else if (kommando.equals("M") || kommando.equals("m"))
+                {
+                    DatagramSocket ds = new DatagramSocket(42069);
+                    byte[] buf = new byte[1024];
+                    DatagramPacket dp = new DatagramPacket(buf, 1024);
+                    while (true)
                     {
-                        DatagramSocket ds = new DatagramSocket(42069);
-                        byte[] buf = new byte[1024];
-                        DatagramPacket dp = new DatagramPacket(buf, 1024);
                         ds.receive(dp);
-                        String str = new String(dp.getData(), 0, dp.getLength());
-                        System.out.println(str);
-                        ds.close();
+                        //String str = new String(dp.getData(), 0, dp.getLength());
+                        System.out.println(dp.getSocketAddress());
                     }
+                    ds.close();
+                }
 
-                    else
-                    {
-                        System.out.println("Forkert input.");
-                    }
-                //}
+
+                else
+                {
+                    System.out.println("Forkert input.");
+                }
+                }
+*/
             }
             catch (Exception e)
             {
