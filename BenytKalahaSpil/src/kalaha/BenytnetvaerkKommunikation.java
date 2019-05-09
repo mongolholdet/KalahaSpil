1f042f042ab6bf2de124866585e6e06f35f1d379
Markus <Markus@DESKTOP-NNP52VT>
2019-05-09 15:46:16 +0200

    Ryddet en smule op

--- BenytKalahaSpil/src/kalaha/BenytnetvaerkKommunikation.java
+++ BenytKalahaSpil/src/kalaha/BenytnetvaerkKommunikation.java
@@ -10,21 +10,21 @@ import java.util.*;
 
 public class BenytnetvaerkKommunikation
 {
+
     public static void main(String[] args) throws Exception
     {
+        String[] IPAdresse = new String[1];
+        IPAdresse[0] = "192.168.0.20";
+        netvaerkKommunikation testForbindelse = new netvaerkKommunikation(IPAdresse, 42069);
         while (true)
         {
             try
             {
                 Scanner input = new Scanner(System.in);
-                System.out.println("Din IP adresse er: " + InetAddress.getLocalHost().getHostAddress()); //Java2s network 
+                System.out.println("Din IP adresse er: " + testForbindelse.getLokalIPAdresse());
                 System.out.println("Vil du modtager eller sende data? [S/M]");
-
                 String kommando = input.nextLine();
-                String[] IPAdresse = new String[1];
-                IPAdresse[0] = "192.168.0.20";
 
-                netvaerkKommunikation testForbindelse = new netvaerkKommunikation(IPAdresse, 42069);
                 if (kommando.equals("S") || kommando.equals("s"))
                 {
                     testForbindelse.sendData("Test af klassen");
