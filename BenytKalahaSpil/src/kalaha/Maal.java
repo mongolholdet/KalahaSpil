a27c53f6e45ea64b14fcf29c0e30f0e1574b82ed
Markus Repnak Jacobsen <markusjacobsen@hotmail.com>
2019-05-10 13:14:36 +0200

    Udfylt Maal klassen som nedarver fra hul

--- BenytKalahaSpil/src/kalaha/Maal.java
+++ BenytKalahaSpil/src/kalaha/Maal.java
@@ -1,15 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package kalaha;
 
-/**
- *
- * @author Markus
- */
 public class Maal extends Hul
 {
     private boolean maal;
+    
+    public String point()
+    {
+        return "Spiller: " + getSpillerNummer() + " har: " + getAntalKugler() + " point.";
+    }
 }
