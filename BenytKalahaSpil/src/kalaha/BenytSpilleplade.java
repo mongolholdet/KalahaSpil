3d1f067229414042f6a28df76b68e7f9c0fef470
Henrik Peter Warncke <henrikpeterwarncke@gmail.com>
2019-05-10 12:50:39 +0200

    Start på spilleplade

--- BenytKalahaSpil/src/kalaha/BenytSpilleplade.java
+++ BenytKalahaSpil/src/kalaha/BenytSpilleplade.java
@@ -1,9 +1,15 @@
 package kalaha;
 
+import javax.swing.JFrame;
+
 public class BenytSpilleplade
 {
     public static void main(String[] args)
     {
-        
+        JFrame vindue = new JFrame("Kalaha");
+	vindue.add(new SpillepladePanel());
+	vindue.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+	vindue.setSize(420,69);
+	vindue.setVisible(true);
     }
 }
