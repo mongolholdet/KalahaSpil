cc4b3bdd0af088a460f95ab06874f8037d02fcc7
Markus <Markus@DESKTOP-NNP52VT>
2019-05-11 11:47:24 +0200

    Spilleplade klassen er i gang med at blive opbygget. genererHuller() virker ikke endnu

--- BenytKalahaSpil/src/kalaha/Spilleplade.java
+++ BenytKalahaSpil/src/kalaha/Spilleplade.java
@@ -3,7 +3,67 @@
  */
 package kalaha;
 
+import java.util.ArrayList;
+
 public class Spilleplade
 {
-    
+
+    private int antalSpillere;
+    private int antalKugler;
+
+    public Spilleplade(int initAntalSpillere, int initAntalKugler)
+    {
+        if (initAntalSpillere == 2)
+        {
+            antalSpillere = initAntalSpillere;
+            antalKugler = initAntalKugler;
+            genererHuller();
+        }
+
+        if (initAntalSpillere == 4)
+        {
+            // Skal implementeres i GUI delen først.
+            antalSpillere = 2;//initAntalSpillere;
+            antalKugler = initAntalKugler;
+            genererHuller();
+        }
+    }
+
+    public int getAntalSpillere()
+    {
+        return antalSpillere;
+    }
+
+    private void genererHuller()
+    {
+        int antalHuller = (antalSpillere * 6) + antalSpillere;
+        System.out.println(antalHuller);
+        //Antallet af huller, med mål inkluderet (6 huller per spiller og et mål per spiller)
+        double spillerNummer = 1;
+        ArrayList<Hul> spillePladeHuller = new ArrayList<Hul>();
+        for (int i = 0; i < antalHuller; i++)
+        {
+
+
+            if (i % 7 == 0)
+            {
+                spillePladeHuller.add(new Maal(0, (int)spillerNummer));
+                            System.out.println("Maal oprettet: " + spillerNummer);
+
+            }
+            else
+            {
+                spillePladeHuller.add(new Hul(antalKugler, (int)spillerNummer));
+            }            spillerNummer = Math.ceil((double)i / (double)7);
+            if (spillerNummer == 0)
+            {
+                spillerNummer = 1;
+            }
+            
+            System.out.println("Spillernummer: " + spillerNummer);
+                        System.out.println("i: " + i);
+
+        }
+        System.out.println(spillePladeHuller);
+    }
 }
