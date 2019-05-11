c70552e875eb75c389dcb13282395b6824d99182
Markus <Markus@DESKTOP-NNP52VT>
2019-05-11 18:13:53 +0200

    oprettet toData funktion til at udsende spilstatus og toString

--- BenytKalahaSpil/src/kalaha/Spilleplade.java
+++ BenytKalahaSpil/src/kalaha/Spilleplade.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 
 public class Spilleplade
 {
-
     private int antalSpillere;
     private int antalKugler;
     private int antalKuglerIMaal;
@@ -25,7 +24,7 @@ public class Spilleplade
 
         if (initAntalSpillere == 4)// Skal implementeres i GUI delen først, laver alligevel en spillerplade med 2 spillere
         {
-            antalSpillere = 2; 
+            antalSpillere = 4;
             antalKugler = initAntalKugler;
             antalKuglerIMaal = initAntalKuglerIMaal;
             genererHuller();
@@ -36,29 +35,24 @@ public class Spilleplade
     {
         return antalSpillere;
     }
-    
-    public String toString()
-    {
-        return spillePladeHuller.toString();
-    }
 
     private void genererHuller()
     {
         int antalHuller = (antalSpillere * 6) + antalSpillere;
         //Antallet af huller, med mål inkluderet (6 huller per spiller og et mål per spiller)
-        
+
         double spillerNummer;
-        
+
         for (int i = 0; i < antalHuller; i++)
         {
-            spillerNummer = Math.ceil((double)i / (double)6); 
+            spillerNummer = Math.ceil((double)i / (double)6);
             // afrundet et kommatal opad til et heltal som repræsenterer en spiller. Alle kommatal mindre og lig med 1 er lig med 1 og alle kommatal større end 1 men mindre eller lig med 2 er lig med 2, osv.
             if (spillerNummer == 0)
             {
                 spillerNummer = 1;
                 // "Doven" løsning til ovenstående spillerNummer kode. Da i starter på 0, kommer det første spillerNummer til at være lig med 0/6, som altså er 0, selvom 1 ønskes.
             }
-            if (i % 7 == 0) 
+            if (i % 7 == 0)
             {
                 spillePladeHuller.add(new Maal(antalKuglerIMaal, (int)spillerNummer));
                 // Hvert syvende hul skal være et Maal objekt
@@ -69,5 +63,20 @@ public class Spilleplade
             }
         }
         //System.out.println(spillePladeHuller);
+    }
+
+    public String toString()
+    {
+        return spillePladeHuller.toString();
+    }
+
+    public String toData()
+    {
+        String data = new String();
+        for (Hul hul : spillePladeHuller)
+        {
+            data += hul.toData();
+        }
+        return data;
     }
 }
