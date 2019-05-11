81f8a8f281ea3abf05caf886e3e8fae09b61dedf
Markus <Markus@DESKTOP-NNP52VT>
2019-05-11 19:09:02 +0200

    Tilføjet hulNummer til klassen

--- BenytKalahaSpil/src/kalaha/Hul.java
+++ BenytKalahaSpil/src/kalaha/Hul.java
@@ -4,17 +4,24 @@ public class Hul
 {
 
     private int antalKugler;
+    private int hulNummer;
     private int spillerNummer;
 
-    public Hul(int initAntalKugler, int initSpillerNummer)
+    public Hul(int initAntalKugler, int initHulNummer, int initSpillerNummer)
     {
         antalKugler = initAntalKugler;
+        hulNummer = initHulNummer;
         spillerNummer = initSpillerNummer;
     }
 
     public int getAntalKugler()
     {
         return antalKugler;
+    }
+
+    public int getHulNummer()
+    {
+        return hulNummer;
     }
 
     public int getSpillerNummer()
@@ -27,6 +34,11 @@ public class Hul
         antalKugler = setAntalKugler;
     }
 
+    public void setHulNummer(int setAntalKugler)
+    {
+        hulNummer = setAntalKugler;
+    }
+
     public void setSpillerNummer(int setSpillerNummer)
     {
         spillerNummer = setSpillerNummer;
@@ -34,11 +46,11 @@ public class Hul
 
     public String toString()
     {
-        return "Hullet har: " + antalKugler + " kugler og tilhører spiller: " + spillerNummer;
+        return "Hullet har: " + antalKugler + " kugler, nummeret: " + hulNummer + " og tilhører spiller: " + spillerNummer;
     }
 
     public String toData()
     {
-        return "[" + spillerNummer + "." + antalKugler + "]";
+        return "[" + spillerNummer + "." + hulNummer + "." + antalKugler + "]";
     }
 }
