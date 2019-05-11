6e74ea33840d8cac1da57cc258766a0cf0410aa3
Markus <Markus@DESKTOP-NNP52VT>
2019-05-11 18:13:24 +0200

    oprettet toData funktion til at udsende spilstatus

--- BenytKalahaSpil/src/kalaha/Hul.java
+++ BenytKalahaSpil/src/kalaha/Hul.java
@@ -2,37 +2,43 @@ package kalaha;
 
 public class Hul
 {
+
     private int antalKugler;
     private int spillerNummer;
-    
+
     public Hul(int initAntalKugler, int initSpillerNummer)
     {
         antalKugler = initAntalKugler;
         spillerNummer = initSpillerNummer;
     }
-    
+
     public int getAntalKugler()
     {
         return antalKugler;
     }
-    
+
     public int getSpillerNummer()
     {
         return spillerNummer;
-    } 
-    
+    }
+
     public void setAntalKugler(int setAntalKugler)
     {
         antalKugler = setAntalKugler;
     }
-    
+
     public void setSpillerNummer(int setSpillerNummer)
     {
         spillerNummer = setSpillerNummer;
-    } 
-    
+    }
+
     public String toString()
     {
         return "Hullet har: " + antalKugler + " kugler og tilhører spiller: " + spillerNummer;
+    }
+
+    public String toData()
+    {
+        return "[" + spillerNummer + "." + antalKugler + "]";
     }
 }
