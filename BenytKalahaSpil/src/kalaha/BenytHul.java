e84a1ec5d1dd5f3d1cda3fba3b592e831192358c
Markus <Markus@DESKTOP-NNP52VT>
2019-05-11 21:00:57 +0200

    Opdateret med nye Hul parametre

--- BenytKalahaSpil/src/kalaha/BenytHul.java
+++ BenytKalahaSpil/src/kalaha/BenytHul.java
@@ -4,9 +4,9 @@ public class BenytHul
 {
     public static void main(String[] args)
     {
-        Hul testHul = new Hul(420, 69);
+        Hul testHul = new Hul(420,1, 69);
         System.out.println(testHul.toString());
-        Maal testMaal = new Maal(420, 69); 
+        Maal testMaal = new Maal(420,1, 69); 
 
         System.out.println(testMaal.point());
         System.out.println(testMaal.getAntalKugler());
