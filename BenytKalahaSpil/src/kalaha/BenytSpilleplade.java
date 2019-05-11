6ee6ef80bb2d81d4f4f75b5290840fb9967f7d61
Markus <Markus@DESKTOP-NNP52VT>
2019-05-11 11:47:57 +0200

    Lavet om til at test spilleplade funktionaliteten i stedet for GUI

--- BenytKalahaSpil/src/kalaha/BenytSpilleplade.java
+++ BenytKalahaSpil/src/kalaha/BenytSpilleplade.java
@@ -9,10 +9,14 @@ public class BenytSpilleplade
 {
     public static void main(String[] args)
     {
+        /*
+        // Yoinker lige dokumentet
         JFrame vindue = new JFrame("Kalaha");
 	vindue.add(new SpillepladePanel());
 	vindue.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 	vindue.setSize(500,500);
 	vindue.setVisible(true);
+        */
+        Spilleplade testSpil = new Spilleplade(2, 6);
     }
 }
