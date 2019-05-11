ed4a988a059dc7d28d48b64d586ef1bac6b392ae
Markus <Markus@DESKTOP-NNP52VT>
2019-05-11 20:59:41 +0200

    Bliver også brugt til at teste Spiller klassen

--- BenytKalahaSpil/src/kalaha/BenytSpilleplade.java
+++ BenytKalahaSpil/src/kalaha/BenytSpilleplade.java
@@ -7,7 +7,8 @@ import javax.swing.JFrame;
 
 public class BenytSpilleplade
 {
-    public static void main(String[] args)
+
+    public static void main(String[] args) throws Exception
     {
         /*
         // Yoinker lige dokumentet
@@ -16,7 +17,15 @@ public class BenytSpilleplade
 	vindue.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 	vindue.setSize(500,500);
 	vindue.setVisible(true);
-        */
-        Spilleplade testSpil = new Spilleplade(2, 6);
+         */
+        Spilleplade testSpil = new Spilleplade(2, 6, 0);
+        System.out.println(testSpil.toString());
+        System.out.println(testSpil.toData());
+        
+        System.out.println("Spiller test:");
+        String[] test = new String[1];
+        test[0] = "test";
+        Spiller testSpiller = new Spiller(test, 41970);
+        testSpiller.modtagSpilStatus();
     }
 }
